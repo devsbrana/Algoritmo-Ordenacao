0454ced1d73444e22c52ed136421bc01e43856f9
devsbrana <sbranaww@gmail.com>
2025-10-10 09:59:42 -0300

    Soma-Lista-Recursiva

---
+++ soma-lista-recursiva.py
@@ -0,0 +1,19 @@
+def soma_lista(lista):
+    soma = 0
+    for i in lista:
+        soma += i
+    return soma
+
+#Função Recursiva 
+def soma_lista_(lista):
+    if len(lista) == 1: #caso base
+        return lista[0]
+    else:
+        return lista[0] + soma_lista_(lista[1:])
+
+
+#main
+lista = [1, 3, 5, 7, 9]
+print(f'Soma: {soma_lista(lista)}')
+print(f'Soma da lista recursiva: {soma_lista_(lista)}')
+
